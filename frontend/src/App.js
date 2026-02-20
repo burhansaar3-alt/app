627f2d7d7154aeff0daebeef3d69e7252df068f1
BURHAN <burhan.saar@trendsyria.com>
2026-02-20 00:01:51 +0000

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,8 +16,10 @@ import WishlistPage from './pages/WishlistPage';
 import ComplaintsPage from './pages/ComplaintsPage';
 import { Toaster } from './components/ui/sonner';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-const API = `${BACKEND_URL}/api`;
+const BACKEND_URL =
+  process.env.REACT_APP_BACKEND_URL || window.location.origin;
+
+const API = ${BACKEND_URL}/api;
 
 export const api = axios.create({
   baseURL: API,
