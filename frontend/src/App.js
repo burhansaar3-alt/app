13563350c39d69f115a6af2b47eeaa329011f257
BURHAN <burhan.saar@trendsyria.com>
2026-02-20 00:52:24 +0000

    Fix api url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,6 @@ import WishlistPage from './pages/WishlistPage';
 import ComplaintsPage from './pages/ComplaintsPage';
 import { Toaster } from './components/ui/sonner';
 
-const BACKEND_URL = 
 const BACKEND_URL = "https://trend-syria.com";
 const API = ${BACKEND_URL}/api;
 
